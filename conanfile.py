5dabe98021ea18d8670c6868699f93f4a8524cca
Robert Burger <robert.burger@dlr.de>
2019-07-02 09:11:56 +0200

    first step rk-5 conversion

--- conanfile.py
+++ conanfile.py
@@ -7,15 +7,9 @@ class MainProject(ConanFile):
     url = f"https://rmc-github.robotic.dlr.de/robotkernel/{name}"
     description = "robotkernel-5 is a modular, easy configurable hardware abstraction framework"
     settings = "os", "compiler", "build_type", "arch"
-    scm = {
-        "type": "git",
-        "url": "auto",
-        "revision": "auto",
-        "submodule": "recursive",
-    }
-
+    exports_sources = "src/*", "README.wiki", "project.properties", "module_pdrouting.pc.in", "Makefile.am", "m4/*", "configure.ac", "LICENSE"
     generators = "pkg_config"
-    requires = "robotkernel/[~=5.0]@robotkernel/unstable"
+    requires = "robotkernel/[~=5.0]@robotkernel/unstable", "service_provider_process_data_inspection/[~=5]@robotkernel/unstable"
 
     def source(self):
         filedata = None
